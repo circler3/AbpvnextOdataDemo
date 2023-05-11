02c33e7ccd88869240bd81c3d15f852ba4621c14
circler3 <circler3@sina.com>
2023-05-11 17:04:10 +0800

    添加项目文件。

---
+++ src/TodoApp.Domain/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace TodoApp
+{
+    public class TodoItem : BasicAggregateRoot<Guid>
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ src/TodoApp.HttpApi/Controllers/TodoAppController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.OData.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Domain.Repositories;
+
+namespace TodoApp.Controllers;
+
+/* Inherit your controllers from this class.
+ */
+public abstract class TodoAppController : AbpControllerBase
+{
+    protected TodoAppController()
+    {
+        LocalizationResource = typeof(TodoAppResource);
+    }
+}
+
+
+public class TodoAppImplController : TodoAppController
+{
+    private readonly ITodoAppService todoAppService;
+
+    public TodoAppImplController(ITodoAppService todoAppService)
+       :base() 
+    {
+        this.todoAppService = todoAppService;
+    }
+
+    [EnableQuery]
+    public async Task<IEnumerable<TodoItemDto>> GetAsync()
+    {
+        var result = await todoAppService.GetListAsync();
+        return result.AsQueryable();
+    }
+}
